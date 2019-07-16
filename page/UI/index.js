99737002a4b1504a8df15fb18f2e067dc080f843
songxingguo <13289899422.qq.com>
2019-07-16 11:39:52 +0800

    feat: UI  页面

--- page/UI/index.js
+++ page/UI/index.js
@@ -5,7 +5,15 @@ Page({
         id: 'swiper',
         name: '轮播',
         open: false,
-        pages: ['scroll-img','scroll-op']
+        pages: [
+          {
+            zh: '图片轮播',
+            url: 'scroll-img/scroll-img'
+          }, {
+            zh: '功能轮播',
+            url: 'scroll-op/scroll-op'
+          }
+        ]
       }
     ]
   },
@@ -13,6 +21,12 @@ Page({
     var id = e.currentTarget.id, list = this.data.list;
     for (var i = 0, len = list.length; i < len; ++i) {
       if (list[i].id == id) {
+        if (list[i].url) {
+          wx.navigateTo({
+            url: 'pages/' + list[i].url
+          })
+          return
+        }
         list[i].open = !list[i].open
       } else {
         list[i].open = false
@@ -23,4 +37,3 @@ Page({
     });
   }
 })
-
